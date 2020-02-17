28db20880d29a371423ac8dc19a826c00fab358f
bagdaad-code <60501347+bagdaad-code@users.noreply.github.com>
2020-02-17 12:35:09 +0100

    Update modele.php

--- On2ixV1/libs/modele.php
+++ On2ixV1/libs/modele.php
@@ -5,61 +5,45 @@
 include_once("maLibSQL.pdo.php");
 
 
-function listerUtilisateurs($classe = "both")
+function listerBagues()
 {
-	// NB : la présence du symbole '=' indique la valeur par défaut du paramètre s'il n'est pas fourni
-	// Cette fonction liste les utilisateurs de la base de données 
-	// et renvoie un tableau d'enregistrements. 
-	// Chaque enregistrement est un tableau associatif contenant les champs 
-	// id,pseudo,blacklist,connecte,couleur
-
-	// Lorsque la variable $classe vaut "both", elle renvoie tous les utilisateurs
-	// Lorsqu'elle vaut "bl", elle ne renvoie que les utilisateurs blacklistés
-	// Lorsqu'elle vaut "nbl", elle ne renvoie que les utilisateurs non blacklistés
-
-	$SQL = "select * from users";
-	if ($classe == "bl")
-		$SQL .= " where blacklist=1";
-	if ($classe == "nbl")
-		$SQL .= " where blacklist=0";
-	
-	// echo $SQL;
+	$SQL = "SELECT * FROM bijoux ";
 	return parcoursRs(SQLSelect($SQL));
-
 }
 
 
-function interdireUtilisateur($idUser)
+function triPrixASC()
 {
-	// cette fonction affecte le booléen "blacklist" à vrai
-	$SQL = "UPDATE users SET blacklist=1 WHERE id='$idUser'";
-	// les apostrophes font partie de la sécurité !! 
-	// Il faut utiliser addslashes lors de la récupération 
-	// des données depuis les formulaires
-
-	SQLUpdate($SQL);
+	$SQL = "SELECT * FROM bijoux  ORDER BY bijoux.prix ASC";
+		return parcoursRs(SQLSelect($SQL));
 }
-
-function autoriserUtilisateur($idUser)
+function triPrixDESC()
 {
-	// cette fonction affecte le booléen "blacklist" à faux 
-	$SQL = "UPDATE users SET blacklist=0 WHERE id='$idUser'";
-	SQLUpdate($SQL);
+	$SQL = "SELECT * FROM `bijoux`  ORDER BY `bijoux`.`prix` DESC";
+		return parcoursRs(SQLSelect($SQL));
 }
-
-function verifUserBdd($login,$passe)
+function triCaratASC()
 {
-	// Vérifie l'identité d'un utilisateur 
-	// dont les identifiants sont passes en paramètre
-	// renvoie faux si user inconnu
-	// renvoie l'id de l'utilisateur si succès
-
-	$SQL="SELECT id FROM users WHERE pseudo='$login' AND passe='$passe'";
-
-	return SQLGetChamp($SQL);
-	// si on avait besoin de plus d'un champ
-	// on aurait du utiliser SQLSelect
+	$SQL = "SELECT * FROM `bijoux`  ORDER BY `bijoux`.`purete` ASC";
+		return parcoursRs(SQLSelect($SQL));
 }
+function triCaratDESC()
+{
+	$SQL = "SELECT * FROM `bijoux`  ORDER BY `bijoux`.`purete` DESC";
+		return parcoursRs(SQLSelect($SQL));
+}
+function triPopularite()
+{
+	$SQL = "SELECT * FROM `bijoux`  ORDER BY `bijoux`.`popularite` ASC";
+		return parcoursRs(SQLSelect($SQL));
+}
+function triCheckbox($eltTri)
+{
+	$SQL = "SELECT * FROM bijoux WHERE alliage LIKE '$eltTri' ";
+		return parcoursRs(SQLSelect($SQL));
+}
+
+
 
 
 ?>
