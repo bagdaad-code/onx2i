f636371f1f9387f357f3be29b17a6494989c2feb
AxelFcqr <60501397+AxelFcqr@users.noreply.github.com>
2020-03-12 09:41:11 +0100

    Add files via upload

--- On2ixV1/templates/surMesure.php
+++ On2ixV1/templates/surMesure.php
@@ -1,14 +1,58 @@
+<!--
+   quiSommesNous.html
+
+   Copyright 2020 afacqueu <afacqueu@b2-IG-201PC20.domaine.lan>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA.
+
+
+-->
 <?php
 
-//C'est la propriété php_self qui nous l'indique : 
-// Quand on vient de index : 
-// [PHP_SELF] => /chatISIG/index.php 
-// Quand on vient directement par le répertoire templates
-// [PHP_SELF] => /chatISIG/templates/accueil.php
+if (basename($_SERVER["PHP_SELF"]) != "index.php")
+{
+	header("Location:../index.php?view=surMesure");
+	die("");
+}
 
-// Si la page est appelée directement par son adresse, on redirige en passant pas la page index
-// Pas de soucis de bufferisation, puisque c'est dans le cas où on appelle directement la page sans son contexte
 ?>
 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
- 
+<head>
+	<title>Sur Mesure</title>
+	<meta charset="UTF-8">
+	<link href="css/serviceClient.css" rel="stylesheet">
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+	<style type="text/css">
+	      [class*="col"] { margin-top: 20px; }
+	      img { width: 70%;
+				text-align:center; }
+	    </style>
+</head>
+
+<body>
+	<div class="container">
+	  <h1 class="titre">Créez votre bijou</h1>
+
+
+	</div>
+
+</body>
+
+</html>
