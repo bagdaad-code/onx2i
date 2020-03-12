ac2881e7ab7922fcd930e052f268c57b22a0713d
h178 <58563143+h178@users.noreply.github.com>
2020-03-12 02:55:18 +0000

    Update panier.php

--- On2ixV1/templates/panier.php
+++ On2ixV1/templates/panier.php
@@ -1,5 +1,209 @@
 
+
 <?php
+session_start();
+
+/*
+Cette page génère les différentes vues de l'application en utilisant des templates situés dans le répertoire "templates". Un template ou 'gabarit' est un fichier php qui génère une partie de la structure XHTML d'une page. 
+
+La vue à afficher dans la page index est définie par le paramètre "view" qui doit être placé dans la chaîne de requête. En fonction de la valeur de ce paramètre, on doit vérifier que l'on a suffisamment de données pour inclure le template nécessaire, puis on appelle le template à l'aide de la fonction include
+
+Les formulaires de toutes les vues générées enverront leurs données vers la page data.php pour traitement. La page data.php redirigera alors vers la page index pour réafficher la vue pertinente, généralement la vue dans laquelle se trouvait le formulaire. 
+*/
+
+
+	include_once "C:\wamp64\www\on2ixv1\libs\maLibUtils.php";
+	include_once "C:\wamp64\www\on2ixv1\libs\maLibBootstrap.php";
+
+
+
+	// on récupère le paramètre view éventuel 
+	$view = valider("view"); 
+	/* valider automatise le code suivant :
+	if (isset($_GET["view"]) && $_GET["view"]!="")
+	{
+		$view = $_GET["view"]
+	}*/
+
+	// S'il est vide, on charge la vue accueil par défaut
+	//if (!$view) $view = "C:\wamp64\www\On2ixV1/templates\accueil.php"; 
+
+	// NB : il faut que view soit défini avant d'appeler l'entête
+
+	// Dans tous les cas, on affiche l'entete, 
+	// qui contient les balises de structure de la page, le logo, etc. 
+	// Le formulaire de recherche ainsi que le lien de connexion 
+	// si l'utilisateur n'est pas connecté 
+
+	include("C:\wamp64\www\on2ixv1/templates/header.php");
+
+	// En fonction de la vue à afficher, on appelle tel ou tel template
+	switch($view)
+	{		
+
+		case "accueil" : 
+			include("templates/accueil.php");
+		break;
+			
+		case "article" : 
+			include("templates/article.php");
+		break;
+			
+		case "bague" : 
+			include("templates/bague.php");
+		break;
+			
+		case "facture" : 
+			include("templates/facture.php");
+		break;
+			
+		case "favoris" : 
+			include("templates/favoris.php");
+		break;
+			
+		case "login" : 
+			include("templates/login.php");
+		break;
+			
+		case "panier" : 
+			include("templates/panier.php");
+		break;
+			
+		case "quiSommesNous" : 
+			include("templates/quiSommesNous.php");
+		break;
+			
+		case "serviceClient" : 
+			include("templates/serviceClient.php");
+		break;
+			
+		case "surMesure" : 
+			include("templates/surMesure.php");
+		break;
+		
+
+		default : // si le template correspondant à l'argument existe, on l'affiche
+			if (file_exists("templates/$view.php"))
+				include("templates/$view.php");
+
+	}
+
+
+	// Dans tous les cas, on affiche le pied de page
+	// Qui contient les coordonnées de la personne si elle est connectée
+?>
+
+<div class="container-fluid">
+    <div class="row px-5">
+        <div class="col-md-7">
+            <div class="shopping-cart">
+                <h6>Mon Panier</h6>
+                <hr>
+
+                 <form action="panier.php" method="get" class="cart-items">
+                    <div class="border rounded">
+                        <div class="row bg-white">
+                            <div class="col-md-3 pl-0">
+                                <img src="C:\wamp64\www\On2ixV1\ressources\logoIG2I.png"  class="img-fluid">
+                            </div>
+                            <div class="col-md-6">
+                                <h5 class="pt-2">product1</h5>
+                                <small class="text-secondary">Seller: dailytuition</small>
+                                <h5 class="pt-2">555$</h5>
+                                <button type="submit" class="btn btn-warning">Save for Later</button>
+                                <button type="submit" class="btn btn-danger mx-2" name="remove">Remove</button>
+                            </div>
+                            <div class="col-md-3 py-5">
+                                <div>
+                                    <button type="button" class="btn bg-light border rounded-circle"><i class="fas fa-minus"></i></button>
+                                    <input type="text" value="1" class="form-control w-25 d-inline">
+                                    <button type="button" class="btn bg-light border rounded-circle"><i class="fas fa-plus"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+
+
+
+
+
+
+
+                <?php
+
+
+
+
+                $total = 0;
+                    if (isset($_SESSION['cart'])){
+                        $product_id = array_column($_SESSION['cart'], 'product_id');
+
+                        $result = $db->getData();
+                        while ($row = mysqli_fetch_assoc($result)){
+                            foreach ($product_id as $id){
+                                if ($row['id'] == $id){
+                                    cartElement($row['product_image'], $row['product_name'],$row['product_price'], $row['id']);
+                                    $total = $total + (int)$row['product_price'];
+                                }
+                            }
+                        }
+                    }else{
+                      //  echo "<h5>Cart is Empty</h5>";
+                    }
+
+                ?>
+
+            </div>
+        </div>
+        <div class="col-md-4 offset-md-1 border rounded mt-5 bg-white h-25">
+
+            <div class="pt-4">
+                <h6>PRICE DETAILS</h6>
+                <hr>
+                <div class="row price-details">
+                    <div class="col-md-6">
+                        <?php
+                            if (isset($_SESSION['cart'])){
+                                $count  = count($_SESSION['cart']);
+                                echo "<h6>Price ($count items)</h6>";
+                            }else{
+                                echo "<h6>Price (0 items)</h6>";
+                            }
+                        ?>
+                        <h6>Delivery Charges</h6>
+                        <hr>
+                        <h6>Amount Payable</h6>
+                    </div>
+                    <div class="col-md-6">
+                        <h6>$<?php echo $total; ?></h6>
+                        <h6 class="text-success">FREE</h6>
+                        <hr>
+                        <h6>$<?php
+                            echo $total;
+                            ?></h6>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
+<?php
+
+
+
+
+
+
+
 //C'est la propriété php_self qui nous l'indique : 
 // Quand on vient de index : 
 // [PHP_SELF] => /chatISIG/index.php 
@@ -11,176 +215,4 @@
 
 // Bouton ajouter panier 
 /*<a href="panier.php?action=ajout&amp;1=<?php echo $s->title; ?>&amp;q=1&amp;p=<?php echo $s->price; ?>"> ajouter panier</a>*/
-
-
-require_once('include/header.php');
-require_once('include/fonction_panier.php');
-
-$erreur = false;
-
-$action = (isset($_POST['action'])?$_POST['action']:(isset($_GET['action'])?$_GET['action']:null));
-
-if($action !== null){
-
-	if(!in_array($action, array('ajout','suppression','refresh')))
-
-		$erreur = true;
-
-		$r = (isset($_POST['r'])?$_POST['r']:(isset($_GET['r'])?$_GET['r']:null));
-		$q = (isset($_POST['q'])?$_POST['q']:(isset($_GET['q'])?$_GET['q']:null));
-		$p = (isset($_POST['p'])?$_POST['p']:(isset($_GET['p'])?$_GET['p']:null));
-
-		$r = preg_replace('#\v#', '', $r);
-
-		$p = floatval($p);
-
-		if(is_array($q)){
-
-			$qteProduit = array();
-
-			$r = 0;
-
-			foreach ($q as $contenu) {
-				
-				$qteProduit[$i++] = intval($contenu);
-			}
-
-		}else{
-
-			$q = intval($q);
-		}
-
-
-	
-}
-
-if (!$erreur) {
-	
-	switch ($action) {
-
-		Case "ajout":
-			
-			ajouterArticle($r,$q,$p);
-
-			break;
-
-		Case "suppression":
-
-			supprimerArticle($r):
-
-			break;
-
-		Case "refresh":
-
-			for ($i=0; $i <count($qteProduit) ; $i++) { 
-			
-				ModifierQTeProduit($_SESSION['panier']['refProduit'][$i], round($qteProduit));
-			}
-
-			break;
-
-		Default:
-			
-			break;
-	}
-}
-
 ?>
-
-<form method ="post" action ="">
-	<table width="400">
-		<tr>
-			<td colspan="4"> Votre panier </td>
-		</tr>
-		<tr>
-			<td> Référence Produit </td>
-			<td> Prix unitaire </td>
-			<td> Quantité </td>
-			<td> Action </td>
-		</tr>
-
-		<?php
-
-			if (isset($_GET['deletepanier']) && $_GET['deletepanier'] == true ) {
-				
-				SupprimerPanier();
-			}
-
-			if (creationPanier()) {
-				
-			
-
-			$nbProduit = count($_SESSION['panier']['refProduit']);
-
-			if($nbProduit <= 0){
-
-				echo '<br/><p style="font-size:22px; color:Red;">Panier Vide !';
-
-			}else{
-
-				for ($i=0; $i <$nbProduit ; $i++) { 
-					
-						?>
-
-						<tr>
-
-							<td></br><?php echo $_SESSION['panier']['refProduit'][$i]; ?></td>
-
-							<td></br><input name="q[]" value="<?php echo $_SESSION['panier']['qteProduixt'][$i];?>"
-							size = "5" /></td>
-							<td></br><?php echo $_SESSION['panier']['prixProduit'][$i];
-							?></td>
-							<td></br><a href="panier.php?action=suppression&amp;1=<?php echo rawurldecode($_SESSION['panier']['refProduit'][$i]); ?>">X</a></td>
-
-							
-
-						</tr>
-
-						<?php 
-					}
-						?>
-
-						<tr>
-							
-							<td colspan="2"><br/>
-								<p> Total : <?php echo MontantGlobal(). " euros"; ?></p><br/>
-						</tr>
-
-						<tr>
-							
-							<td colspan="4"><br/>
-								<input type="submit" name="rafraichir"/>
-								<input type="hidden" name="action" value="refresh"/>
-								<a href="?deletepanier=true">Supprimer panier</a>
-
-								
-							</td>
-
-						</tr>
-
-
-						<?php
-				}
-
-			
-
-		}
-
-		?>	
-	</table>
-	
-
-</form>
-
-<?php
-
-	require_once('includes/footer.php');
-	
-?>
-
-
- 
-
-
-
- 
