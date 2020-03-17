d2f579467350649bc99256690109734567405df3
AxelFcqr <60501397+AxelFcqr@users.noreply.github.com>
2020-03-17 21:02:57 +0100

    Add files via upload

--- On2ixV2/controleur.php
+++ On2ixV2/controleur.php
@@ -1,44 +1,66 @@
 <?php
+
 session_start();
 
 	include_once "libs/maLibUtils.php";
 	include_once "libs/maLibSQL.pdo.php";
-	include_once "libs/maLibSecurisation.php"; 
-	include_once "libs/modele.php"; 
+	include_once "libs/maLibSecurisation.php";
+	include_once "libs/modele.php";
 
+	header("Access-Control-Allow-Origin: *");
+	header("Access-Control-Allow-Methods: *");
+	header("Access-Control-Allow-Headers: *");
 	$addArgs = "";
 
 	if ($action = valider("action"))
 	{
 		ob_start ();
 		echo "Action = '$action' <br />";
-		// ATTENTION : le codage des caractères peut poser PB si on utilise des actions comportant des accents... 
+		// ATTENTION : le codage des caractères peut poser PB si on utilise des actions comportant des accents...
 		// A EVITER si on ne maitrise pas ce type de problématiques
 
 		/* TODO: A REVOIR !!
-		// Dans tous les cas, il faut etre logue... 
+		// Dans tous les cas, il faut etre logue...
 		// Sauf si on veut se connecter (action == Connexion)
 
-		if ($action != "Connexion") 
+		if ($action != "Connexion")
 			securiser("login");
 		*/
 
 		// Un paramètre action a été soumis, on fait le boulot...
 		switch($action)
 		{
-			
-			
+			//Mail pour le sur mesure/////////////////////////////////
+			case 'Mail sur mesure' :
+				// Le message
+
+				$style = $_POST["selStyle"];
+				$alliage = $_POST["selAlliage"];
+				$purete = $_POST["selPurete"];
+				$pierre = $_POST["selPierre"];
+				$taille = $_POST["selTaille"];
+				$mail = $_POST["mailEnvoyeur"];
+				$message = "Commande de $mail\r\nStyle de la bague : $style\r\n Alliage : $alliage\r\n Pureté : $purete\r\n Pierre : $pierre\r\n Taille : $taille";
+
+				// Dans le cas où nos lignes comportent plus de 70 caractères, nous les coupons en utilisant wordwrap()
+				$message = wordwrap($message, 70, "\r\n");
+
+				// Envoi du mail
+				mail('super-marsien@hotmail.fr', 'Commande sur mesure ', $message);
+				//une fois le mail envoyé on est redirigé vers la vue index.php
+			break;
+
 			// Connexion //////////////////////////////////////////////////
 			case 'Connexion' :
 				// On verifie la presence des champs login et passe
 				if ($login = valider("login"))
 				if ($passe = valider("passe"))
 				{
-					// On verifie l'utilisateur, 
+					// On verifie l'utilisateur,
 					// et on crée des variables de session si tout est OK
 					// Cf. maLibSecurisation
 					if (verifUser($login,$passe)) {
-						// tout s'est bien passé, doit-on se souvenir de la personne ? 
+						// tout s'est bien passé, doit-on se souvenir de la personne ?
 						if (valider("remember")) {
 							setcookie("login",$login , time()+60*60*24*30);
 							setcookie("passe",$password, time()+60*60*24*30);
@@ -49,7 +71,7 @@ session_start();
 							setcookie("remember",false, time()-3600);
 						}
 
-					}	
+					}
 				}
 
 				// On redirigera vers la page index automatiquement
@@ -77,15 +99,5 @@ session_start();
 
 	// On écrit seulement après cette entête
 	ob_end_flush();
-	
+
 ?>
-
-
-
-
-
-
-
-
-
-
