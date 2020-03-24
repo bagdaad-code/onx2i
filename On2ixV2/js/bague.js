07f32b4f3894f10ba837dd975b6f03bafef607e8
bagdaad-code <60501347+bagdaad-code@users.noreply.github.com>
2020-03-24 15:17:13 +0100

    Update bague.js

--- On2ixV2/js/bague.js
+++ On2ixV2/js/bague.js
@@ -2,7 +2,9 @@
 var apiRoot = "http://localhost/on2ix/api";
 var listeBaguesPossible=[];
 var listeBaguesAffichee=[];
-var dejaTri=false;
+var listeBaguesAfficher=[];
+var dejaTri=0;
+var quimapl=0;
 /*------------------------Declachement lorsque la page est chargé---------------------------------------*/
 $(document).ready(function(){
   $("#titre").html("Bienvenue Dans La Galerie de vos Bagues");
@@ -11,6 +13,8 @@ $(document).ready(function(){
 });
 /*-----------Declachement au click-------------------*/
 $(document).on('click', '.option', triSelectBagues);
+$(document).on('click', '.buttonFav', mettreFav);
+$(document).on('click', '.buttonShop', mettreShop);
 $(document).on('click', '.checkalliage', triButtonBagues);
 $(document).on('click', '.checkgenre', triCheckBagues);
 $(document).on('change', '.selectPrix', triPrixBagues);
@@ -19,13 +23,13 @@ $(document).on('change', '.selectCarat', triCaratBagues);
 //permet d'afficher les bagues
 function afficherBagues(){
   $("#bague").html("");
-
-  dejaTri=false;
   for (var i in listeBaguesPossible) {
     var div=$("<div>")
     var h3=$("<h3>")
     var img=$("<img>")
     var p=$("<p>")
+	   var btn=$("<button class=\"buttonFav\"> <i class=\"fa fa-heart\"></i> </button>").attr('type','button')
+	   var btn2=$("<button class=\"buttonShop\"> <i class=\"fa fa-shopping-basket\"></i> </button>").attr('type','button')
     div.attr({"id":listeBaguesPossible[i].id,"class":"bagues"});
     img.attr("src",listeBaguesPossible[i].source);
     p.text("a partir de "+listeBaguesPossible[i].prix+" €");
@@ -33,6 +37,8 @@ function afficherBagues(){
     div.append(img)
     div.prepend(h3);
     div.append(p);
+    div.append(btn)
+    div.append(btn2)
     $("#bague").append(div);
   }
 }
@@ -40,55 +46,64 @@ function afficherBagues(){
 //permet d'afficher les bagues suite a un tri
 function afficherBaguesTri(criTri,tripar1,tripar2,info){
   var nb=0;
-  if (dejaTri) {
-    listeBaguesAAfficher=listeBaguesAffichee;
+  if (dejaTri>0){
+    listeBaguesAfficher=listeBaguesAffichee;
   }else {
-    listeBaguesAAfficher=listeBaguesPossible
+    listeBaguesAfficher=listeBaguesPossible;
+    listeBaguesAffichee=[];
   }
-  for (var i in listeBaguesPossible) {
+  for (var i in listeBaguesAfficher) {
     var div=$("<div>")
     var h3=$("<h3>")
     var img=$("<img>")
     var p=$("<p>")
+    var btn=$("<button class=\"buttonFav\"> <i class=\"fa fa-heart\"></i> </button>").attr('type','button')
+    var btn2=$("<button class=\"buttonShop\"> <i class=\"fa fa-shopping-basket\"></i> </button>").attr('type','button')
     if (info=="test") {
-      if (listeBaguesPossible[i][criTri]==tripar1) {
-            listeBaguesAffichee.push(listeBaguesPossible[i]);
+      if (listeBaguesAfficher[i][criTri]==tripar1) {
+            listeBaguesAffichee.push(listeBaguesAfficher[i]);
         nb++;
-        div.attr({"id":listeBaguesPossible[i].id,"class":"bagues"});
-        img.attr("src",listeBaguesPossible[i].source);
-        p.text("a partir de "+listeBaguesPossible[i].prix+" €");
-        h3.html(listeBaguesPossible[i].titre);
+        div.attr({"id":listeBaguesAfficher[i].id,"class":"bagues"});
+        img.attr("src",listeBaguesAfficher[i].source);
+        p.text("a partir de "+listeBaguesAfficher[i].prix+" €");
+        h3.html( listeBaguesAfficher[i].titre);
         div.append(img)
         div.prepend(h3);
         div.append(p);
+       div.append(btn)
+       div.append(btn2)
         $("#bague").append(div);
       }
     }
     if (info=="entre") {
-      if (tripar1<=parseFloat(listeBaguesPossible[i][criTri]) && parseFloat(listeBaguesPossible[i][criTri]) <= tripar2){
-            listeBaguesAffichee.push(listeBaguesPossible[i]);
+      if (tripar1<=parseFloat( listeBaguesAfficher[i][criTri]) && parseFloat( listeBaguesAfficher[i][criTri]) <= tripar2){
+            listeBaguesAffichee.push( listeBaguesAfficher[i]);
         nb++;
-        div.attr({"id":listeBaguesPossible[i].id,"class":"bagues"});
-        img.attr("src",listeBaguesPossible[i].source);
-        p.text("a partir de "+listeBaguesPossible[i].prix+" €");
-        h3.html(listeBaguesPossible[i].titre);
+        div.attr({"id": listeBaguesAfficher[i].id,"class":"bagues"});
+        img.attr("src", listeBaguesAfficher[i].source);
+        p.text("a partir de "+ listeBaguesAfficher[i].prix+" €");
+        h3.html( listeBaguesAfficher[i].titre);
         div.append(img)
         div.prepend(h3);
         div.append(p);
+       div.append(btn)
+       div.append(btn2)
         $("#bague").append(div);
       }
     }
   }
-
+  dejaTri=1;
+  listeBaguesAffichee = Array.from(new Set(listeBaguesAffichee));
   return nb;
 }
 /*----------CacherBaguesTri----------*/
 //permet de cacher les bagues suite a un tri par genre
 function CacherBaguesTri(genre){
-  for (var i in listeBaguesPossible) {
-    if (listeBaguesPossible[i].genre==genre) {
-    var id=listeBaguesPossible[i].id;
+  for (var i in  listeBaguesAfficher) {
+    if ( listeBaguesAfficher[i].genre==genre) {
+    var id=listeBaguesAfficher[i].id;
       $("#"+id).remove();
+              dejaTri=0;
     }
   }
 }
@@ -134,19 +149,26 @@ function triSelectBagues (){
 //permet de lister les bijoux avec un filtre button
 function triButtonBagues (){
   var nb;
-
+  quimapl=1;
+  console.log(9446);
   if (this.id=="raz") {
       listerBagues();
+        dejaTri=0;
   } else {
-           $("#bague").html("");
+    if (quimapl==1 || quimapl==0 ){
+      dejaTri=0;
+    }else {
+      quimapl=1;
+    }
+     $("#bague").html("");
     nb=afficherBaguesTri("alliage",this.id,0,"test");
-    console.log(nb);
     if (nb==0) {
       listerBagues();
 			$("#err").show();
 			setTimeout(function () {
 				$("#err").hide();
 			}, 1000);
+        dejaTri=0;
     }
   }
 }
@@ -154,11 +176,13 @@ function triButtonBagues (){
 //permet de afficher ou cacher les bijoux selon le sexe choisi
 function triCheckBagues(){
 var nb;
-console.log(!$("#F").is(":checked"));
-console.log(!$("#M").is(":checked"));
-console.log(!$("#U").is(":checked"));
-if (!$("#F").is(":checked") && !$("#M").is(":checked") && !$("#U").is(":checked"))
-afficherBagues();
+quimapl=3
+console.log(dejaTri);
+if (!$("#F").is(":checked") && !$("#M").is(":checked") && !$("#U").is(":checked")){
+  afficherBagues();
+            dejaTri=0;
+}
+
 
   if (this.checked) {
     if ($("#F").is(":checked") && $("#M").is(":checked") ||
@@ -171,8 +195,11 @@ afficherBagues();
               $("#err").hide();
 
             }, 1000);
+                    dejaTri=0;
              $("#"+this.id).prop('checked', false);
           }
+
+
         }
         else {
        $("#bague").html("");
@@ -184,11 +211,16 @@ afficherBagues();
          setTimeout(function () {
            $("#err").hide();
          }, 1000);
+                 dejaTri=0;
        }
-        }
-
+     }
+     if (quimapl==3){
+       dejaTri=0;
+     }else {
+       dejaTri=1;
+     }
   }else {
-    if (!$("#F").is(":checked")) CacherBaguesTri("F");
+    if (!$("#F").is(":checked"))CacherBaguesTri("F");
     if (!$("#M").is(":checked")) CacherBaguesTri("M");
     if (!$("#U").is(":checked")) CacherBaguesTri("U");
     if (!$("#F").is(":checked") && !$("#M").is(":checked") && !$("#U").is(":checked"))
@@ -199,6 +231,7 @@ afficherBagues();
 //permet de afficher ou cacher les bijoux selon la tranche de prix choisi
 function triPrixBagues(){
   var nb;
+  quimapl=4
   var min=parseFloat( $("#selectPrixMin").children(":selected").val() );
   var max=parseFloat( $("#selectPrixMax").children(":selected").val() );
   $("#bague").html("");
@@ -211,6 +244,13 @@ function triPrixBagues(){
     setTimeout(function () {
       $("#err").hide();
     }, 1000);
+            dejaTri=0;
+  }else{
+    if (quimapl==4){
+      dejaTri=0;
+    }else {
+      dejaTri=1;
+    }
   }
 
 }
@@ -218,6 +258,7 @@ function triPrixBagues(){
 //permet de afficher ou cacher les bijoux selon la tranche de carat choisi
 function triCaratBagues(){
   var nb;
+  quimapl=5;
   var min=parseFloat( $("#selectCaratMin").children(":selected").val() );
   var max=parseFloat( $("#selectCaratMax").children(":selected").val() );
   $("#bague").html("");
@@ -228,6 +269,20 @@ function triCaratBagues(){
     setTimeout(function () {
       $("#err").hide();
     }, 1000);
-  }
+            dejaTri=0;
+  }else{
+    if (quimapl==5){
+      dejaTri=0;
+    }else {
+      dejaTri=1;
+    }
+}
+}
+function mettreShop (){
+  console.log(this);
+
+}
+function mettreFav (){
+  console.log(this);
 
 }
