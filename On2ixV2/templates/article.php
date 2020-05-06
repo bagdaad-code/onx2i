3f63af5eeeb0f7c83a9f8953529763744297ac23
Y-KaOne <60501652+Y-KaOne@users.noreply.github.com>
2020-05-06 15:41:56 +0200

    Update article.php

--- On2ixV2/templates/article.php
+++ On2ixV2/templates/article.php
@@ -1,11 +1,81 @@
-<?php
 
-//C'est la propriété php_self qui nous l'indique :
-// Quand on vient de index :
-// [PHP_SELF] => /chatISIG/index.php
-// Quand on vient directement par le répertoire templates
-// [PHP_SELF] => /chatISIG/templates/accueil.php
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="truc.js" charset="utf-8"></script>
+<!--- Include the above in your HEAD tag ---------->
 
-// Si la page est appelée directement par son adresse, on redirige en passant pas la page index
-// Pas de soucis de bufferisation, puisque c'est dans le cas où on appelle directement la page sans son contexte
-?>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+<link rel="stylesheet" href="truc.css">
+
+<main>
+
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="card mb-10">
+
+          <div class="contrainer">
+
+
+          <div class="card-body store-body">
+            <div class="product-info">
+              <h4 class="product-title mb-2">Bague entourage en or blanc avec un saphir ovale et diamants ronds</h4>
+              <h2 class="product-price display-4">1325.00 €</h2>
+              <div class="product-gallery">
+                <div class="product-gallery-thumbnails">
+                  <ol class="thumbnails-list list-unstyled">
+                    <li><img src="ressources/fiance.jpg" alt=""></li>
+                  </ol>
+                </div>
+
+                <div class="product-payment-details">
+
+                  <p class="text-success"><i class="fa fa-credit-card"></i>inclut 20% TVA</p>
+                  <p class="mb-0"><i class="fa fa-truck"></i> Livraison international </p>
+                  <div class="text-muted mb-2"><small>information concernant la livraison</small></div>
+                  <label for="quant">Quantité</label>
+                  <input type="number" name="quantity" min="1" id="quant" class="form-control mb-5 input-lg" placeholder="Choisir la quantité">
+                  <button class="btn btn-primary btn-lg btn-block">Ajouter au panier</button>
+
+                </div>
+
+              </div>
+<br>
+                <div class="product-description mb-5">
+                  <h2 class="mb-5">Caractéristiques :</h2>
+                  <dl class="row mb-5">
+                    <dt class="col-sm-3">Alliage</dt>
+                    <dd class="col-sm-9">Or blanc 750 ‰ </dd>
+                    <dt class="col-sm-3">Poids de carats</dt>
+                    <dd class="col-sm-9">0.85 Ct</dd>
+                    <dt class="col-sm-3">Qualité du diamant </dt>
+                    <dd class="col-sm-9">G couleur, VS2 pureté, Qualité de la taille VG</dd>
+                    <dt class="col-sm-3">Taille de la bague </dt>
+                    <dd class="col-sm-9">52</dd>
+                  </dl>
+                  <h2 class="mb-5">Description</h2>
+                  <p>L'alliage de ce bijou est composée à 75% d'or pur, également appelé or 18 carats ou or 750 ‰. Cela garantit que votre bijou est à la fois solide et durable. Pour obtenir la couleur moderne de l'or blanc, l'or a été allié au cuivre et à l'argent.</p>
+                  <p>Le serti cathédrale est un serti moderne et élégant pour une bague avec un diamant solitaire. Des rainures sont créées dans le côté du diamant par le biais de l'inscription au laser, le métal précieux de la bague exerce ensuite une pression sur le diamant. Le diamant reste alors en place grâce à la pression.</p>
+                </div>
+                <div class="product-faq mb-5">
+                  <h2 class="mb-3">Avez-vous des quesions ?</h2>
+                  <p class="text-muted">De quelles informations avez-vous besoin?</p>
+                  <div class="main-questions d-inline" data-container="body" data-toggle="popover" data-placement="right" data-content="Are you in doubt? these shortcuts can help you!">
+                    <a href="#" class="btn btn-outline-primary">Prix et livraison</a>
+                    <a href="#" class="btn btn-outline-primary">Garrantie</a>
+                    <a href="#" class="btn btn-outline-primary">Option de paiment</a>
+                  </div>
+                </div>
+                <div class="product-comments">
+                  <h5 class="mb-2"><i class="fa fa-comment"></i> Demandez à Sami</h5>
+                  <form action="" class="form-inline mb-5">
+                    <textarea name="" id="" cols="50" rows="2" class="form-control mr-4" placeholder="Ecrire la question"></textarea><button class="btn btn-lg btn-primary">Envoyer</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
